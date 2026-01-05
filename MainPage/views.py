d47e7f21484e205cd0f888b5d421eebea129cf6d
Вова Дубинка <dubinkavovchik406@gmail.com>
2026-01-05 22:51:26 +0200

    refactor: organizated templates into subdirectories

--- MainPage/views.py
+++ MainPage/views.py
@@ -10,7 +10,7 @@ from django.contrib.auth import login
 
 
 class CustomLoginView(LoginView):
-    template_name = 'Group_portal/login.html'
+    template_name = 'users/login.html'
     redirect_authenticated_user = True
     next_page = reverse_lazy('group-list')
 
@@ -18,7 +18,7 @@ class CustomLogoutView(LogoutView):
     next_page = reverse_lazy('login')
 
 class RegisterView(FormView):
-    template_name = 'Group_portal/register.html'
+    template_name = 'users/register.html'
     form_class = forms.CustomUserCreationForm
     success_url = reverse_lazy('group-list')
 
@@ -30,38 +30,38 @@ class RegisterView(FormView):
 class GroupListView(LoginRequiredMixin, ListView):
     # це списки "груп" людей
     model = Group
-    template_name = "Group_portal/group_list.html"
+    template_name = "groups/group_list.html"
     context_object_name = "groups"
 
 class GroupDetailView(LoginRequiredMixin, DetailView):
     # це для 1 конкретної групи
     model = Group
-    template_name = "Group_portal/group_detail.html"
+    template_name = "groups/group_detail.html"
     context_object_name = "group"
 
 class GroupCreateView(LoginRequiredMixin, IsStaffMixin, CreateView):
     # створення групи
     model = Group
     form_class = forms.GroupForm
-    template_name = "Group_portal/group_create.html"
+    template_name = "groups/group_create.html"
     success_url = reverse_lazy("group-list")
 
 class GroupUpdateView(LoginRequiredMixin, IsStaffMixin, UpdateView):
     model = Group
     form_class = forms.GroupForm
-    template_name = "Group_portal/group_update.html"
+    template_name = "groups/group_update.html"
     context_object_name = "group"
     success_url = reverse_lazy("group-list")
 
 class GroupDeleteView(LoginRequiredMixin, IsStaffMixin, DeleteView):
     model = Group
     success_url = reverse_lazy("group-list")
-    template_name = "Group_portal/group_delete.html"
+    template_name = "groups/group_delete.html"
 
 class JoinGroupView(LoginRequiredMixin, UpdateView):
     model = CustomUser
     form_class = forms.JoinGroupForm
-    template_name = "Group_portal/join_group.html"
+    template_name = "users/join_group.html"
     success_url = reverse_lazy("group-list")
 
     # Юзер может менять группу только самому себе(Кто бы ни зашел на эту страницу, всегда доставай из базы того, кто сейчас авторизован)
@@ -71,7 +71,7 @@ class JoinGroupView(LoginRequiredMixin, UpdateView):
 class UserUpdateView(LoginRequiredMixin, UserIsOwnerMixin, UpdateView):
     model = CustomUser
     form_class = forms.CustomUserUpdateForm
-    template_name = "Group_portal/user_update.html"
+    template_name = "users/user_update.html"
 
     def get_success_url(self):
         return reverse("user-detail", kwargs={"pk": self.object.pk})
@@ -79,17 +79,17 @@ class UserUpdateView(LoginRequiredMixin, UserIsOwnerMixin, UpdateView):
 class UserDeleteView(LoginRequiredMixin, UserIsOwnerMixin, DeleteView):
     model = CustomUser
     success_url = reverse_lazy("user-list")
-    template_name = "Group_portal/user_delete.html"
+    template_name = "users/user_delete.html"
 
 class UserListView(LoginRequiredMixin, ListView):
     # список користувачів
     model = CustomUser
-    template_name = "Group_portal/user_list.html"
+    template_name = "users/user_list.html"
     context_object_name = "users"
 
 class UserDetailView(LoginRequiredMixin, DetailView):
     # 1 користувач
     model = CustomUser
-    template_name = "Group_portal/user_detail.html"
+    template_name = "users/user_detail.html"
     context_object_name = "user"
 
