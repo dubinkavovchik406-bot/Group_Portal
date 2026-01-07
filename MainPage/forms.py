5d81734621e6fb78a41bbe4e5c706957f4618e9d
Вова Дубинка <dubinkavovchik406@gmail.com>
2026-01-07 19:47:10 +0200

    refactor(templates): add bootstrap style in templates, forms and main_css style

--- MainPage/forms.py
+++ MainPage/forms.py
@@ -13,13 +13,33 @@ class CustomUserUpdateForm(forms.ModelForm):
         model = CustomUser
         fields = ["email", "first_name", "last_name", "age", "gender"]
 
-class JoinGroupForm(forms.ModelForm):
-    class Meta:
-        model = CustomUser
-        fields = ("group", )
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Проходим циклом по всем полям и добавляем им нужные классы
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'form-control btn-round-sm border-2',
+                'placeholder': field.label # добавим плейсхолдеры вместо пустых полей
+            })
 
 
 class GroupForm(forms.ModelForm):
     class Meta:
         model = Group
         fields = ["name", "about"]
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'form-control shadow-sm',
+                'placeholder': 'Наприклад: Любителі котів'
+            }),
+            'about': forms.Textarea(attrs={
+                'class': 'form-control shadow-sm',
+                'rows': 5,
+                'placeholder': 'Розкажіть про що ваша спільнота...',
+                'style': 'resize: none;'
+            }),
+        }
+        labels = {
+            'name': 'Назва спільноти',
+            'about': 'Про групу',
+        }
